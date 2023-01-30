0632bcf7d020e197ae3627718ea56a1c01513d74
SanghwaJ <jungsh930210@gmail.com>
2023-01-30 17:29:16 +0900

    edit

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -191,7 +191,6 @@ public class ValidationItemControllerV2 {
             return "validation/v2/addForm";
         }
 
-        // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
